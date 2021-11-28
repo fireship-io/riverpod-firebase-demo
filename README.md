24a5f6ada484b4a2bbaf4465a83c0c7815f2f047
Jeff Delaney <hello@fireship.io>
2021-11-27 19:36:58 -0700

    full working demo

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# riverfire
+# FlutterFire + Riverpod
 
-A new Flutter project.
+This repo contains an example using [FlutterFire](https://pub.dev/packages/flutter_fire) and [riverpod](https://pub.dev/packages/riverpod) to manage realtime relational data streams. 
 
-## Getting Started
+- Listen to Firebase Auth State
+- Listen to realtime Firestore data based on current user's UID
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Checkout the full [Riverpod Firebase Tutorial](https://fireship.io/lessons/firebase-riverpod-flutter/) for a full explanation and video. 
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,103 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-// We create a "provider", which will store a value (here "Hello world").
-// By using a provider, this allows us to mock/override the value exposed.
 final helloWorldProvider = Provider((_) => 'Hello world');
 
-final userProvider =
-    Provider<User?>((ref) => FirebaseAuth.instance.currentUser);
-
-final userProvider2 =
-    StreamProvider<User?>((ref) => FirebaseAuth.instance.authStateChanges());
+final userProvider = StreamProvider<User?>(
+  (ref) => FirebaseAuth.instance.authStateChanges(),
+);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
-    // For widgets to be able to read providers, we need to wrap the entire
-    // application in a "ProviderScope" widget.
-    // This is where the state of our providers will be stored.
     ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
-// Note: MyApp is a HookConsumerWidget, from flutter_hooks.
 class MyApp extends ConsumerWidget {
-  final Future<FirebaseApp> _init = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // To read our provider, we can use the hook "ref.watch(".
-    // This is only possible because MyApp is a HookConsumerWidget.
-    final String value = ref.watch(helloWorldProvider);
+    final String helloWorld = ref.watch(helloWorldProvider);
 
-    final user = ref.watch(userProvider2);
+    final user = ref.watch(userProvider);
 
-    return user.when(
-      data: (user) {
-        return MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(title: Text('Example')),
-            body: Center(
-              child: Text(user?.uid ?? 'not signed in'),
+    // return MaterialApp(
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text(helloWorld),
+    //     ),
+    //   ),
+    // );
+
+      return user.when(
+        data: (user) {
+          return MaterialApp(
+            home: Scaffold(
+              appBar: AppBar(title: Text(helloWorld)),
+              body: Column(
+                children: [Text(user?.uid ?? 'not signed in'), AccountDetails()],
+              ),
+              floatingActionButton: FloatingActionButton(
+                child: user != null ? Icon(Icons.outbond) : Icon(Icons.person),
+                onPressed: () async {
+                  if (user != null) {
+                    FirebaseAuth.instance.signOut();
+                  } else {
+                    var credential =
+                        await FirebaseAuth.instance.signInAnonymously();
+                    var ref = FirebaseFirestore.instance
+                        .collection('accounts')
+                        .doc(credential.user?.uid);
+                    ref.set({'hello': 'my account!!!'});
+                  }
+                },
+              ),
             ),
-            floatingActionButton: FloatingActionButton(
-              child: user != null ? Icon(Icons.outbond) : Icon(Icons.person),
-              onPressed: () {
-                if (user != null) {
-                  FirebaseAuth.instance.signOut();
-                } else {
-                  FirebaseAuth.instance.signInAnonymously();
-                }
-              },
-            ),
-          ),
-        );
+          );
+        },
+        error: (e, s) => Text('error'),
+        loading: () => Text('loading'),
+      );
+  }
+}
+
+
+final dataProvider = StreamProvider<Map?>(
+  (ref) {
+    final userStream = ref.watch(userProvider);
+
+    var user = userStream.asData?.value;
+
+    if (user != null) {
+      var docRef =
+          FirebaseFirestore.instance.collection('accounts').doc(user.uid);
+      return docRef.snapshots().map((doc) => doc.data());
+    } else {
+      return Stream.empty();
+    }
+  },
+);
+
+
+// Listen to data in Firestore
+class AccountDetails extends ConsumerWidget {
+  @override
+  Widget build(context, ref) {
+    final data = ref.watch(dataProvider);
+
+    return data.when(
+      data: (account) {
+        return Text(account?['hello'] ?? 'empty');
       },
       error: (e, s) => Text('error'),
-      loading: () => Text('loading'),
+      loading: () => Text('waiting for data...'),
     );
   }
 }
